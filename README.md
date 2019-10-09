064ab4d32c0b5056ae950e3c1d832ae96d1e17b6
DaniLatreCodes <danilatre.skycode@gmail.com>
2019-10-09 18:09:22 +0200

    Readme added

---
+++ README.md
@@ -0,0 +1,23 @@
+#Brastlewark app
+
+![D3](https://img.shields.io/badge/0.19.0-axios-green)
+![D3](https://img.shields.io/badge/4.12.0-node-sass-red)
+![D3](https://img.shields.io/badge/16.10.0-react-Yellow)
+![D3](https://img.shields.io/badge/2.6.0-nodeFetch-orange)
+![D3](https://img.shields.io/badge/8.5.1-jsonwebtoken-violet)
+
+## Description
+
+🚀This's React.js web app wich feeds from a placeholder a show it in a responsive way.
+
+You can see all players in game, search for any of them, check the detail, and from ther go to another friend player
+
+##[Live Demo](http://brastlewark_dani.surge.sh)
+
+
+### Start React local Server
+
+
+```
+npm start
+```
---
+++ brastlewark-app/build/precache-manifest.11280b7e84ce2d9c4dc187f25a61a4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f242b61533b8b071d49f7bc2b3280c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e347b1d9381b9ebc908c",
+    "url": "/static/css/main.a98a4a5d.chunk.css"
+  },
+  {
+    "revision": "69cbcafe116d8267ca8c",
+    "url": "/static/js/2.767a63fe.chunk.js"
+  },
+  {
+    "revision": "e347b1d9381b9ebc908c",
+    "url": "/static/js/main.b12a72cf.chunk.js"
+  },
+  {
+    "revision": "fb2a88b811b9fd1ded64",
+    "url": "/static/js/runtime-main.5807942b.js"
+  },
+  {
+    "revision": "9a2ea1c5f878396ac7466aab2457f1d8",
+    "url": "/static/media/logo.9a2ea1c5.png"
+  }
+]);
